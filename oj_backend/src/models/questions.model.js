54eab9e001187b1db57a0c16489f585cca3bcab7
Hemendra Maheta <91376520+hmehta051@users.noreply.github.com>
2024-05-12 23:38:30 +0530

    Create questions.model.js

---
+++ oj_backend/src/models/questions.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true, unique: true },
+    difficulty: { type: String, required: true },
+    constraints: { type: String, required: true },
+    topic: { type: String, required: true },
+    initialCode: { type: String, required: true },
+    testCase: [
+      {
+        input: { type: String, required: true },
+        output: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const QuestionsModel = new mongoose.model("Question",questionSchema);
+
+module.exports=QuestionsModel
