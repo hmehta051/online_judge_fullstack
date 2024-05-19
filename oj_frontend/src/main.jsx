00b3ca473f16cac9aad59ad2d00589cbf5bde669
Hemendra Maheta <91376520+hmehta051@users.noreply.github.com>
2024-05-19 15:27:26 +0530

    Update main.jsx

--- oj_frontend/src/main.jsx
+++ oj_frontend/src/main.jsx
@@ -4,12 +4,15 @@ import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
 import App from "./App.jsx";
 import "./index.css";
-
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
-      <App />
-    </FluentProvider>
+    <Provider store={store}>
+      <FluentProvider theme={webLightTheme}>
+        <App />
+      </FluentProvider>
+    </Provider>
   </React.StrictMode>
 );
