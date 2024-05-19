a2e062085aa46d43233d7b956b4ed1ea288e115f
Hemendra Maheta <91376520+hmehta051@users.noreply.github.com>
2024-05-19 15:24:28 +0530

    Update Navbar.jsx

--- oj_frontend/src/components/Navbar.jsx
+++ oj_frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full h-full shadow-lg p-4">
-      <img src="./logo.png" alt="CodeKing Logo" width={200} height={200} className="w-[150px] md:w-[200px] sm:w-[150px]"/>
+      <img src="https://ik.imagekit.io/bksgfrlfg/logo_daad_dAnA.png?updatedAt=1716035561396" alt="CodeKing Logo" width={200} height={200} className="w-[150px] md:w-[200px] sm:w-[150px]"/>
     </div>
   );
 };
